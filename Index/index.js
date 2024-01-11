15f568ac9966c3980cd2ad630e37ae6f8e22b2e2
matheus <matheusoficial1411@gmail.com>
2024-01-11 08:39:25 -0300

    V.iniciada

---
+++ Index/index.js
@@ -0,0 +1,8 @@
+function AbrirModal(fundo,modal){
+    fundo.style.display="block";
+    modal.style.display="block";
+}
+function FecharModal(fundo,modal){
+    fundo.style.display="none";
+    modal.style.display="none";
+}
