2d0e18b4bcd273ce32c618c31ff1ef8970390af0
Guilherme_Araujo <tecgui.informatica@gmail.com>
2024-02-01 07:22:24 -0300

    V.5.1

--- BackEnd/sessao.php
+++ BackEnd/sessao.php
@@ -1,14 +1,17 @@
 <?php
 include_once "conexao.php";
 session_start();
+//Constantes de ambiente
+//Legado - Remover posteriormente
+const SESSION_USER_ID = "UserId";
+const SESSION_USERNAME = "UserName";
+const SESSION_CPF = "UserCpf";
 $db = new Conexao();
 if (isset($_GET['loginSucess'])) {
     $email = $_GET['email'];
     $dados = $db->executar("SELECT id, first_name, last_name, email, employee FROM users WHERE email = '$email'");
 }
-class sessao{
-    function obterNome($nome, $sobrenome){
-        $nomeCompleto = $nome . ' ' . $sobrenome;
-        return $nomeCompleto;
-    }
+function getName()
+{
+    return getName();
 }
