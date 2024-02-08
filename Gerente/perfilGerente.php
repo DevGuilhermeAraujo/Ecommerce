86131cf131d1512b5bf152d973a20a23c82b33e9
matheus <matheusoficial1411@gmail.com>
2024-02-08 08:00:01 -0300

    08/02/24.1

--- Gerente/perfilGerente.php
+++ Gerente/perfilGerente.php
@@ -8,10 +8,13 @@
     <link rel="stylesheet" href="gerente.css">
 </head>
 <body>
+
+    <!--Painel com o perfil de gerente-->
    <div class="craxa">
         <img class="imgPerfil" src="../Imagens/Fundos/fundoPrincipal.jpg" alt="imagem de perfil">
         <div class="infoPerfil">
             <h2>Nome completo</h2>
+            <p>CPF: <span>000.000.000-00</span></p>
             <p>Idade: <span>00</span></p>
             <p>Cargo: <span>Gerente</span></p>
             <p>Endereço: <span>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</span></p>
@@ -19,5 +22,15 @@
             <button>Editar</button>
         </div>
    </div>
+
+   <!--Fundo para os modais-->
+   <div style="display: none;" id="perfilGerente" class="fundoModal"></div>
+
+    <!--Modal para modificar perfil-->
+    <div id="modificarPerfil" class="modal">
+        <img onclick="FecharModal()" class="fecharModal" src="../Imagens/Icones/Fechar.png" alt="icone fechar">
+        <h2><img src="" alt=""> Modificar perfil <img src="" alt=""></h2>
+    </div>
+
 </body>
 </html>
